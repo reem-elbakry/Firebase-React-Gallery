604b6adbf5f9245d4fd536292916fbccca956b49
reem-elbakry <reemelbakry95@gmail.com>
2022-08-08 22:27:44 +0200

    add env sample file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,12 +9,12 @@ import "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBZ_LxurdXV8U1PzAP3obrvXMiyiALeqH4",
-  authDomain: "gallery-76fd6.firebaseapp.com",
-  projectId: "gallery-76fd6",
-  storageBucket: "gallery-76fd6.appspot.com",
-  messagingSenderId: "671691853172",
-  appId: "1:671691853172:web:09765a3de7a686081e3021",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
